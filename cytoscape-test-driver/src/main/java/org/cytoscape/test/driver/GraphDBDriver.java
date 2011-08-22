a8192e8b4e4707fec4a5c020209826366c00955f
Keiichiro Ono <keiono@gmail.com>
2011-08-21 23:32:19 -0700

    DB location string extracted.

--- cytoscape-test-driver/src/main/java/org/cytoscape/test/driver/GraphDBDriver.java
+++ cytoscape-test-driver/src/main/java/org/cytoscape/test/driver/GraphDBDriver.java
@@ -10,17 +10,18 @@ public class GraphDBDriver {
 	
     private static final Logger logger = LoggerFactory.getLogger(GraphDBDriver.class);
     
+    // Modify this! 
+    private static final String DB_LOCATION = "/Users/k/sail1";
+    
     GraphDBDriver() {
     	// Simply creates new databases.
     	
     	logger.info("\n\n\n********* Graph DB Driver start **********\n\n\n");
     	
-    	final NativeStoreSailGraph sailGraph = new NativeStoreSailGraph("/Users/k/sail1");
+    	final NativeStoreSailGraph sailGraph = new NativeStoreSailGraph(DB_LOCATION);
     	final Sail rawGraph = sailGraph.getRawGraph();
     	sailGraph.shutdown();
     	
     	logger.info("\n\n\n############# Graph DB Driver Success: " + rawGraph.toString() + "\n\n\n");
     }
-
-
 }
